d6ebcac5548e876590b4179c46c77f89f918b9a3
imhajes <imhajes@gmail.com>
2018-08-17 23:08:24 +0300

    Update bot.rb

--- bot.rb
+++ bot.rb
@@ -6,9 +6,7 @@ $bot = Cinch::Bot.new do
   configure do |c|
     c.server   = "irc.freenode.org"
     c.user     = "w3c4"
-    c.nick     = ["fornet87,
-                  saudi6554,
-                  liban8779"]
+    c.nick     = "w3c4_"
     c.realname = "0,2 The 7w3c4 0,2 Bot 13v1 0,3 Saudi Arabia 3,30"
     c.channels = ["#w3c4-bot"]
   end

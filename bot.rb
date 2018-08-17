4533f6fc7e46ac61f673950acce288ace0805cff
imhajes <imhajes@gmail.com>
2018-08-17 22:55:17 +0300

    Update bot.rb

--- bot.rb
+++ bot.rb
@@ -6,7 +6,9 @@ $bot = Cinch::Bot.new do
   configure do |c|
     c.server   = "irc.dal.net"
     c.user     = "w3c4"
-    c.nick     = "fornet87, saudi6554, liban8779"
+    c.nick     = ["fornet87,
+                  saudi6554,
+                  liban8779"]
     c.realname = "0,2 The 7w3c4 0,2 Bot 13v1 0,3 Saudi Arabia 3,30"
     c.channels = ["#king"]
   end

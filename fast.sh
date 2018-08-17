00ece1944f1cd42dad997b03f0fb9abf615b3bf7
imhajes <imhajes@gmail.com>
2018-08-17 22:30:20 +0300

    Update fast.sh

--- fast.sh
+++ fast.sh
@@ -1,25 +1,25 @@
-heroku create saudi34
-heroku git:remote -a saudi34
+heroku create w3c4-bot111
+heroku git:remote -a w3c4-bot111
 git push heroku master
 heroku scale web=0
 heroku scale bot=1
-heroku create saudi344
-heroku git:remote -a saudi344
+heroku create w3c4-bot112
+heroku git:remote -a w3c4-bot112
 git push heroku master
 heroku scale web=0
 heroku scale bot=1
-heroku create kuwaiti3445
-heroku git:remote -a kuwaiti3445
+heroku create w3c4-bot113
+heroku git:remote -a w3c4-bot113
 git push heroku master
 heroku scale web=0
 heroku scale bot=1
-heroku create omani34
-heroku git:remote -a omani34
+heroku create w3c4-bot114
+heroku git:remote -a w3c4-bot114
 git push heroku master
 heroku scale web=0
 heroku scale bot=1
-heroku create uae4432
-heroku git:remote -a uae4432
+heroku create w3c4-bot115
+heroku git:remote -a w3c4-bot115
 git push heroku master
 heroku scale web=0
 heroku scale bot=1

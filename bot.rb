83c2162add51ff48e3ecaae11868d926e12d3431
imhajes <imhajes@gmail.com>
2018-08-19 00:32:10 +0300

    Update bot.rb

--- bot.rb
+++ bot.rb
@@ -5,6 +5,7 @@ require 'cinch'
 $bot = Cinch::Bot.new do
   configure do |c|
     c.server   = "eu.undernet.org"
+    c.port     = 6667
     c.user     = "w3c4"
     c.nick     = "s#{rand(100000)}"
     c.realname = "0,2 The 7w3c4 0,2 Bot 13v1 0,3 Saudi Arabia 3,30"

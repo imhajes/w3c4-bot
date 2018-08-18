92d18ac2646b6839d8cfb7d4fd5942cf7a0a3e7c
imhajes <imhajes@gmail.com>
2018-08-19 00:36:14 +0300

    Update bot.rb

--- bot.rb
+++ bot.rb
@@ -1,6 +1,7 @@
 #! /usr/bin/env ruby
 
 require 'cinch'
+require "cinch/plugins/basic_ctcp"
 
 $bot = Cinch::Bot.new do
   configure do |c|
@@ -10,7 +11,9 @@ $bot = Cinch::Bot.new do
     c.nick     = "s#{rand(100000)}"
     c.realname = "0,2 The 7w3c4 0,2 Bot 13v1 0,3 Saudi Arabia 3,30"
     c.channels = ["#w3c4"]
+    c.plugins.plugins = [Cinch::Plugins::BasicCTCP] # optionally add more plugins
   end
+end
   
   on :message, "!run" do |m|
     m.reply "0,2 The 7w3c4 0,2 Bot 13v1 0,3 zZzZz 3,30 "

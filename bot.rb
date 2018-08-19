b4ceb2c91522c8e8c6be31f2dfca6cf2e27476e3
imhajes <imhajes@gmail.com>
2018-08-19 18:20:55 +0300

    Update bot.rb

--- bot.rb
+++ bot.rb
@@ -1,13 +1,12 @@
 #! /usr/bin/env ruby
 
 require 'cinch'
-require "cinch/plugins/basic_ctcp"
 
 $bot = Cinch::Bot.new do
   configure do |c|
-    c.server   = "irc.dal.net"
-    c.user     = "w3c4"
-    c.nick     = [
+    c.server = 'irc.freenode.net'
+    c.user = 'kittybot'
+    c.nick = [
       "TubeStatus",
       "Karma",
       "TheGrads",
@@ -22,12 +21,9 @@ $bot = Cinch::Bot.new do
       "Kicker"
     ]
     c.realname = "0,2 The 7w3c4 0,2 Bot 13v1 0,3 Saudi Arabia 3,30"
-    c.channels = ["#go"]
-    c.plugins.options[Cinch::Plugins::BasicCTCP][:replies] = {
-    :version => 'mIRC v7.52'
-  }
-end
-
+    c.channels = ['#go']
+  end
+  
   
   on :message, "!run" do |m|
     m.reply "0,2 The 7w3c4 0,2 Bot 13v1 0,3 zZzZz 3,30 "

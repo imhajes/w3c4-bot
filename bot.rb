d8940d9bec0a528055533da5294a186cf6923a15
imhajes <imhajes@gmail.com>
2018-08-19 18:04:10 +0300

    Update bot.rb

--- bot.rb
+++ bot.rb
@@ -7,7 +7,20 @@ $bot = Cinch::Bot.new do
   configure do |c|
     c.server   = "irc.dal.net"
     c.user     = "w3c4"
-    c.nick     = "saudi-ctcp"
+    c.nick     = [
+      "TubeStatus",
+      "Karma",
+      "TheGrads",
+      "Cat",
+      "LunchRoulette",
+      "Weather",
+      "CelebTracker",
+      "Metrolink",
+      "News",
+      "Anonymous",
+      "Nick",
+      "Kicker"
+    ]
     c.realname = "0,2 The 7w3c4 0,2 Bot 13v1 0,3 Saudi Arabia 3,30"
     c.channels = ["#go"]
     c.plugins.options[Cinch::Plugins::BasicCTCP][:replies] = {

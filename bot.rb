9d6b3d3136c05251bfce75cf24328ce12dfbf769
imhajes <imhajes@gmail.com>
2018-08-20 17:28:59 +0300

    Update bot.rb

--- bot.rb
+++ bot.rb
@@ -6,7 +6,7 @@ $bot = Cinch::Bot.new do
   configure do |c|
     c.server = 'irc.dal.net'
     c.user = 'nohacks'
-    c.nick = 'kingbot'
+    c.nick = 'gitbot#{rand(100000)}'
     c.realname = "0,2 The 7w3c4 0,2 Bot 13v1 0,3 Saudi Arabia 3,30"
     c.channels = ['#go']
   end

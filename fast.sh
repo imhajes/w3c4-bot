1bd85387758a709f632c9464a0a125352a5098f7
imhajes <imhajes@gmail.com>
2018-08-20 18:50:28 +0300

    Update fast.sh

--- fast.sh
+++ fast.sh
@@ -1,40 +1,25 @@
-heroku create w3c4-bot11111
-heroku git:remote -a w3c4-bot11111
-bundle update
-git add Gemfile.lock
-git commit -m 'Update Gemfile.lock for Heroku'
+heroku create saudi-bot1
+heroku git:remote -a saudi-bot1
 git push heroku master
 heroku scale web=0
 heroku scale bot=1
-heroku create w3c4-bot11222
-heroku git:remote -a w3c4-bot11222
-bundle update
-git add Gemfile.lock
-git commit -m 'Update Gemfile.lock for Heroku'
+heroku create saudi-bot2
+heroku git:remote -a saudi-bot2
 git push heroku master
 heroku scale web=0
 heroku scale bot=1
-heroku create w3c4-bot11333
-heroku git:remote -a w3c4-bot11333
-bundle update
-git add Gemfile.lock
-git commit -m 'Update Gemfile.lock for Heroku'
+heroku create saudi-bot3
+heroku git:remote -a saudi-bot3
 git push heroku master
 heroku scale web=0
 heroku scale bot=1
-heroku create w3c4-bot11444
-heroku git:remote -a w3c4-bot11444
-bundle update
-git add Gemfile.lock
-git commit -m 'Update Gemfile.lock for Heroku'
+heroku create saudi-bot4
+heroku git:remote -a saudi-bot4
 git push heroku master
 heroku scale web=0
 heroku scale bot=1
-heroku create w3c4-bot11555
-heroku git:remote -a w3c4-bot11555
-bundle update
-git add Gemfile.lock
-git commit -m 'Update Gemfile.lock for Heroku'
+heroku create saudi-bot5
+heroku git:remote -a saudi-bot5
 git push heroku master
 heroku scale web=0
 heroku scale bot=1

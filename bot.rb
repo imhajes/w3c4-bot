026a623088920c67a3f025ca36b8391e7d58fdf0
imhajes <imhajes@gmail.com>
2018-08-22 20:05:13 +0300

    Update bot.rb

--- bot.rb
+++ bot.rb
@@ -2,19 +2,36 @@
 
 require 'cinch'
 
+$admin = "injekt"
+
 $bot = Cinch::Bot.new do
   configure do |c|
-    c.server = 'irc.freenode.net'
-    c.user = 'w3c4'
+    c.server = 'irc.dal.net'
     c.nick = (0...8).map { (65 + rand(26)).chr }.join
     c.realname = "0,2 The 7w3c4 0,2 Bot 13v1 0,3 Saudi Arabia 3,30"
     c.channels = ['#w3c4-bot']
   end
   
   
-  on :message, "!run" do |m|
-    m.reply "0,2 The 7w3c4 0,2 Bot 13v1 0,3 zZzZz 3,30 "
+ helpers do
+    def is_admin?(user)
+      true if user.nick == $admin
+    end
+  end
+
+  on :message, /^!join (.+)/ do |m, channel|
+    bot.join(channel) if is_admin?(m.user)
+  end
+
+  on :message, /^!part(?: (.+))?/ do |m, channel|
+    # Part current channel if none is given
+    channel = channel || m.channel
+
+    if channel
+      bot.part(channel) if is_admin?(m.user)
+    end
   end
 end
 
-$bot.start
+bot.start
+

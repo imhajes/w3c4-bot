aedc3e4be8b1b283e270debb620c862ea0da344e
imhajes <imhajes@gmail.com>
2018-08-22 21:00:46 +0300

    Update bot.rb

--- bot.rb
+++ bot.rb
@@ -7,7 +7,7 @@ $admin = "hajes"
 
 bot = Cinch::Bot.new do
   configure do |c|
-    c.server   = "irc.dal.org"
+    c.server   = "irc.dal.net"
     c.nick     = (0...8).map { (65 + rand(26)).chr }.join
     c.realname = "0,2 The 7w3c4 0,2 Bot 13v1 0,3 Saudi Arabia 3,30"
     c.channels = ["#Gold"]

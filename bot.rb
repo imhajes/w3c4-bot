39c8179e69a42a92123745a573914f5a9d6d7964
imhajes <imhajes@gmail.com>
2018-08-22 20:48:26 +0300

    Update bot.rb

--- bot.rb
+++ bot.rb
@@ -2,18 +2,18 @@
 
 require 'cinch'
 
-$admin = "hajes"
+# Who should be able to access these plugins
+$admin = "injekt"
 
-$bot = Cinch::Bot.new do
+bot = Cinch::Bot.new do
   configure do |c|
-    c.server = 'irc.dal.net'
-    c.nick = (0...8).map { (65 + rand(26)).chr }.join
+    c.server   = "irc.dal.org"
+    c.nick     = (0...8).map { (65 + rand(26)).chr }.join
     c.realname = "0,2 The 7w3c4 0,2 Bot 13v1 0,3 Saudi Arabia 3,30"
-    c.channels = ['#Gold']
+    c.channels = ["#Gold"]
   end
-  
-  
- helpers do
+
+  helpers do
     def is_admin?(user)
       true if user.nick == $admin
     end

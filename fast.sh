ff459250d2873b78978d12acb6addf4f033b2cc0
imhajes <imhajes@gmail.com>
2018-08-22 18:50:01 +0300

    Update fast.sh

--- fast.sh
+++ fast.sh
@@ -1,25 +1,25 @@
-heroku create saudi-bot1
-heroku git:remote -a saudi-bot1
+heroku create saudi-bot1s
+heroku git:remote -a saudi-bot1s
 git push heroku master
 heroku scale web=0
 heroku scale bot=1
-heroku create saudi-bot2
-heroku git:remote -a saudi-bot2
+heroku create saudi-bot2s
+heroku git:remote -a saudi-bot2s
 git push heroku master
 heroku scale web=0
 heroku scale bot=1
-heroku create saudi-bot3
-heroku git:remote -a saudi-bot3
+heroku create saudi-bot3s
+heroku git:remote -a saudi-bot3s
 git push heroku master
 heroku scale web=0
 heroku scale bot=1
-heroku create saudi-bot4
-heroku git:remote -a saudi-bot4
+heroku create saudi-bot4s
+heroku git:remote -a saudi-bot4s
 git push heroku master
 heroku scale web=0
 heroku scale bot=1
-heroku create saudi-bot5
-heroku git:remote -a saudi-bot5
+heroku create saudi-bot5s
+heroku git:remote -a saudi-bot5s
 git push heroku master
 heroku scale web=0
 heroku scale bot=1

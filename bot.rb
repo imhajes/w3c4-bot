3c767822d794d91c9f12c5bb43f97af120f225dc
imhajes <imhajes@gmail.com>
2019-06-29 18:59:25 +0300

    Update bot.rb

--- bot.rb
+++ bot.rb
@@ -3,15 +3,15 @@
 require 'cinch'
 
 # Who should be able to access these plugins
-$admin = "hajes"
+$admin = "a0ther"
 
 bot = Cinch::Bot.new do
   configure do |c|
     c.server   = "irc.dal.net"
     c.nick     = (0...8).map { (65 + rand(26)).chr }.join
-    c.user     = "gold"
-    c.realname = "0,2 The 7w3c4 0,2 Bot 13v1 0,3 Saudi Arabia 3,30"
-    c.channels = ["#Gold"]
+    c.user     = "a0thera"
+    c.realname = "0,2 The 7a0ther 0,2 Bot 13v1 0,3 Saudi Arabia 3,30"
+    c.channels = ["##a0ther"]
   end
 
   helpers do
